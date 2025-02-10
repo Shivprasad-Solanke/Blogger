905c85e774f62a2925f60cfd990ecc419bf5c1e4
Rushikesh Gorte <gorterushi@gmail.com>
2025-02-11 01:08:25 +0530

    Dockerfile Added

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Step 1: Use a base Python image
+FROM python:3.10-slim
+
+# Step 2: Set working directory in the container
+WORKDIR Blogger/app
+#Install dependencies, including nginx
+RUN apt-get update && apt-get install -y nginx && apt-get clean
+
+# Step 3: Copy application files into the container
+COPY . /Blogger/
+COPY config/nginx.conf /etc/nginx/nginx.conf
+
+# Step 4: Copy the requirements.txt file from the root directory
+COPY requirements.txt /app/requirements.txt
+
+# Step 5: Install Python dependencies
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+# Step 6: Expose ports
+# Port 80 for Nginx and FastAPI
+EXPOSE 80
+
+# Step 7: Start Uvicorn (FastAPI) and Nginx together
+CMD ["sh", "-c", "uvicorn main:app --host 0.0.0.0 --port 8000 & nginx -g 'daemon off;'"]
--- static/js/shared/navbar.js
+++ static/js/shared/navbar.js
@@ -24,8 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 <div id="user-sidebar" class="sidebar">
                     <ul>
-                    <li class="write"><a href="/templates/write.html"><i class="fas fa-pen-to-square write"></i> Write</a></li>
                     <li><a href="/templates/home.html"><i class="fas fa-home"></i>Home</a></li>
+                    <li class="write"><a href="/templates/write.html"><i class="fas fa-pen-to-square write"></i> Write</a></li>
                         <li><a href="/templates/dashboard.html"><i class="fas fa-table-list"></i>Dashboard</a></li>
                         <li><a href="/templates/profile.html"><i class="fas fa-user"></i>Profile</a></li>
                         <li><a href="#" id="sign-out"><i class="fas fa-arrow-right-from-bracket"></i>Sign Out</a></li>
